94c4a902dc3de97668041568c4c722db73da3860
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-10-24 13:12:49 +0300

    Add PostgresStore

--- src/main/scala/io/github/asakaev/util/MongoStore.scala
+++ src/main/scala/io/github/asakaev/util/MongoStore.scala
@@ -5,12 +5,13 @@ import zio.{Task, ZIO}
 object MongoStore {
   final case class Document[K, V](_id: K, value: V, version: Long)
 
-  val live: Store[Int, String] = new Store[Int, String] {
-    def update(k: Int)(f: Option[String] => Task[String]): Task[Unit] = {
+  def occ[K, V]: Store[K, V] = new Store[K, V] {
+    def update(k: K)(f: Option[V] => Task[V]): Task[Unit] = {
       // read data with version by key
       // apply f to data value
       // increment version
       // conditional write on version back
+
       ZIO.fail(new Throwable("NotImplemented"))
     }
   }
---
+++ src/main/scala/io/github/asakaev/util/PostgresStore.scala
@@ -0,0 +1,28 @@
+package io.github.asakaev.util
+
+import zio.{Task, ZIO}
+
+object PostgresStore {
+
+  def singleRow[K, V]: Store[K, V] = new Store[K, V] {
+    def update(k: K)(f: Option[V] => Task[V]): Task[Unit] = {
+      // select for update data by k
+      // apply f to data
+      // write back
+
+      ZIO.fail(new Throwable("NotImplemented"))
+    }
+  }
+
+  def joins[K, V]: Store[K, V] = new Store[K, V] {
+    def update(k: K)(f: Option[V] => Task[V]): Task[Unit] = {
+      // compute lock key as hash(aggregate name + k) -> Long
+      // use transaction-level advisory lock on key
+      // select data from pg
+      // apply f to data
+      // write back
+
+      ZIO.fail(new Throwable("NotImplemented"))
+    }
+  }
+}
