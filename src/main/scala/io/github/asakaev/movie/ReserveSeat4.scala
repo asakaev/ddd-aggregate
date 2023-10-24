17dad7d3713b9d4016153f64b1250a091d2714b1
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-10-24 10:48:16 +0300

    Add ReserveSeat4

---
+++ src/main/scala/io/github/asakaev/movie/ReserveSeat4.scala
@@ -0,0 +1,21 @@
+package io.github.asakaev.movie
+
+import io.github.asakaev.movie.model.{HallId, SeatId, ViewerId}
+import io.github.asakaev.util.Store
+import zio.Task
+
+object ReserveSeat4 {
+  type ReserveSeats = Props => Task[Unit]
+
+  final case class Props(hid: HallId, sids: Set[SeatId], vid: ViewerId)
+  final case class Hall(id: HallId, seats: Map[SeatId, ViewerId])
+
+  def reserve(h: Hall, p: Props): Task[Hall] = ???
+
+  def make(halls: Store[HallId, Hall]): ReserveSeats =
+    props =>
+      halls.update(props.hid) {
+        case None    => reserve(Hall(props.hid, Map.empty), props)
+        case Some(h) => reserve(h, props)
+      }
+}
---
+++ src/main/scala/io/github/asakaev/movie/model/HallId.scala
@@ -0,0 +1,3 @@
+package io.github.asakaev.movie.model
+
+final case class HallId(value: Int) extends AnyVal
