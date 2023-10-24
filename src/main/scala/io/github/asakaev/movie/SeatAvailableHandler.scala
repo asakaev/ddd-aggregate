0d2b0d9a6f3cafbcef8e40e755c7e96cf6beb587
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-10-24 10:58:45 +0300

    Add SeatAvailableHandler

---
+++ src/main/scala/io/github/asakaev/movie/SeatAvailableHandler.scala
@@ -0,0 +1,21 @@
+package io.github.asakaev.movie
+
+import io.github.asakaev.movie.model.{HallId, SeatId, ViewerId}
+import io.github.asakaev.util.Store
+import zio.{Task, ZIO}
+
+object SeatAvailableHandler {
+  type SeatAvailableHandler = SeatAvailable => Task[Unit]
+
+  final case class SeatAvailable(hid: HallId, sid: SeatId)
+  final case class Hall(id: HallId, seats: Map[SeatId, ViewerId])
+
+  def free(h: Hall, event: SeatAvailable): Hall = ???
+
+  def make(halls: Store[HallId, Hall]): SeatAvailableHandler =
+    event =>
+      halls.update(event.hid) {
+        case None    => ZIO.succeed(Hall(event.hid, Map.empty))
+        case Some(h) => ZIO.succeed(free(h, event))
+      }
+}
